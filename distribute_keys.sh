8b3d77508d9eceb4da9ad6762cd8809c032b8f36
Götz Rieger <grieger@redhat.com>
2017-05-24 08:13:03 +0200

    Update distribute_keys.sh

--- distribute_keys.sh
+++ distribute_keys.sh
@@ -1,6 +1,10 @@
 #/bin/bash
 
+logfile="/var/log/${0}.log"
 password=$1
+
+logfile=$$.log
+exec > $logfile 2>&1
 
 for i in master.example.com infra.example.com node1.example.com node2.example.com
 do
